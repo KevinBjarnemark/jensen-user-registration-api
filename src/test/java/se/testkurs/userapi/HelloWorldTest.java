cfbf465b2197c7a6c0e706c3f2b2cedaf0350f0e
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2026-09-09 10:23:18 +0200

    tests: add simple JUnit test

---
+++ src/test/java/se/testkurs/userapi/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package se.testkurs.userapi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    void additionIsCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
